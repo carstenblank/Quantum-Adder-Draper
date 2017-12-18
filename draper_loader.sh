8fa27f513392d66c6d6efbb4e0ae10a251586b85
Carsten Blank <blank@data-cybernetics.com>
2017-12-18 17:01:40 +0100

    Fix python path.

--- draper_loader.sh
+++ draper_loader.sh
@@ -6,6 +6,6 @@ if [[ "$exists" -gt "0" ]]; then
     echo "Draper Benchmark process is running...";
 else
     echo "Starting Draper Benchmark test...";
-    screen -d -m -S draper_benchmark bash -c "cd $HOME/PycharmProjects/quantum-adder-draper/jobs && python3.6 draper_benchmark.py -real &>> ../output/draper.log"
+    screen -d -m -S draper_benchmark bash -c "cd $HOME/PycharmProjects/quantum-adder-draper/jobs && /usr/local/bin/python3.6 draper_benchmark.py -real &>> ../output/draper.log"
     screen -list
 fi;
